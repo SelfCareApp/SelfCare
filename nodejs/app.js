3fe704106304b13fbcd3772ff786c5b3e1889434
Creig Fortune <fortunephiri004@gmail.com>
2019-01-18 22:24:08 -0500

    restructure of project. separating front end from backend

--- nodejs/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.listen(port,()=>{console.log(`server started. port: ${port}`)});
-
-//test route
-app.get('/',(req, res)=>{res.send('Hello SelfCare')});
-
