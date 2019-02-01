515e7a315b5d805bc0047066500a0860980eaf7c
Creig Fortune <fortunephiri004@gmail.com>
2019-01-31 21:09:44 -0500

    added react navigation as project dependancy

--- reactNative/SelfCare/src/screens/Main.js
+++ reactNative/SelfCare/src/screens/Main.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image,ScrollView} from 'react-native';
 import { CardSection,Header} from  '../components/common';
-import {Button,Card,Icon, ListItem} from 'react-native-elements';
+
+import {Button,Card,Icon, ListItem, Avatar, Divider} from 'react-native-elements';
+
 
 
 class MainScreen extends Component{
     render(){
         return (<View style={style.containerStyle}>
-                        <Header headerText="Professionals"/>
-                        <Card title='Magic Mike'
+        
+                        <Card title="@realSlim"
+                              style={style.cardStyle}
                               image={{uri:'https://d95vll9kevyvb.cloudfront.net/5pikaXYBvt4hQ-t3Br7PwpuCnr0=/13x0:787x533/580x0/https%3A//smallslivestatic.s3.amazonaws.com/artist_images/jonathan-barber.jpg'}} >
-                            <Text style={{marginBottom: 10}}>
+                            <Text style={{marginBottom: 20}}>
                                 Real Barbers are born not made.! #masterBarber#mikeHair
                             </Text>
-                            <Button
-                                // icon={<Icon name='code' color='#ffffff' />}
+    
+                            <Button 
+                                icon={<Icon name='code' color='#ffffff' />}
                                 backgroundColor='#03A9F4'
                                 buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                                title='Book Now' />
+                                title='VIEW NOW' />
                         </Card>
         </View>)
     }
@@ -26,6 +30,7 @@ class MainScreen extends Component{
 const style ={
     containerStyle:{
         marginTop:10
-    }
+    },
+
 }
 export default MainScreen;
